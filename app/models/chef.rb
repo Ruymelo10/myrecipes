6b22ee8877001805bb8a25309d5e650d2d76a56a
Ruymelo10 <ruyovidio@gmail.com>
2020-08-25 11:57:24 -0300

    add edit action with tests

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -5,5 +5,5 @@ class Chef < ApplicationRecord
     validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: false }
     has_many :recipes
     has_secure_password 
-    validates :password, presence: true, length: {minimum: 5}
-end
+    validates :password, presence: true, length: {minimum: 5}, allow_nil: true
+end  
---
+++ test/integration/chefs_edit_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class ChefsEditTest < ActionDispatch::IntegrationTest
+  def setup
+  @chef = Chef.create!(chefname: 'ruy', email:'ruy@example.com',
+                       password: "password", password_confirmation:"password")
+  end
+
+  test "reject invalid edit" do
+    get edit_chef_path(@chef)
+    assert_template 'chefs/edit'
+    patch chef_path(@chef), params: {chef: {chefname: " ", email: "ruy@example.com"}}
+    assert_template 'chefs/edit'
+    assert_select 'h2.card-title'
+    assert_select 'div.card-body'
+  end
+
+  test "accept valid signup" do
+    get edit_chef_path(@chef)
+    assert_template 'chefs/edit'
+    patch chef_path(@chef), params: {chef: {chefname: "ruyzin", email: "ruyzin@example.com"}}
+    assert_redirected_to @chef
+    assert_not flash.empty?
+    @chef.reload
+    assert_match "ruyzin", @chef.chefname
+    assert_match "ruyzin@example.com", @chef.email
+  end
+
+end
